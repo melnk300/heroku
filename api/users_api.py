2191a9ed947892d69352d7cd206f8bd63d09274d
melnk300 <melnk300@gmail.com>
2021-04-17 16:38:58 +0500

    Check methods moved to user models

--- api/users_api.py
+++ api/users_api.py
@@ -1,6 +1,4 @@
 from flask import jsonify, abort, request, Blueprint
-from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required
-
 
 from pprint import pprint
 
@@ -13,68 +11,30 @@ from data.models.Groups import Group
 blueprint = Blueprint('users_api', __name__)
 
 
-def if_group_not_found(group_id):
-    sess = db_session.create_session()
-    group = sess.query(Group).get(group_id)
-    if not group:
-        abort(404, 'Group id not found')
-
-
-def if_user_not_found_by_id(user_id):
-    sess = db_session.create_session()
-    user = sess.query(User).get(user_id)
-    if not user:
-        abort(404, 'User id not found')
-
-
-def if_user_not_found_by_name(user_name):
-    sess = db_session.create_session()
-    user = sess.query(User).filter(User.login == user_name).all()
-    if not user:
-        abort(404, 'User name not found')
-
-
-def if_user_not_found_by_group(user_group_id):
-    sess = db_session.create_session()
-    user = sess.query(User).filter(User.group_id == user_group_id).all()
-    if not user:
-        abort(404, 'User with this group_id name not found')
-
-
-def if_user_already_created(user_name):
-    sess = db_session.create_session()
-    user = sess.query(User).filter(User.login == user_name).all()
-    if user:
-        abort(403, 'User already created')
-
 
 @blueprint.route('/api/users/<group_id>', methods=['GET'])
 def users_by_group(group_id):
-    if_group_not_found(group_id)
-    if_user_not_found_by_group(group_id)
+    User.if_user_not_found_by_group(group_id)
     sess = db_session.create_session()
     users = sess.query(User.id, User.login, Group.name).filter(User.group_id == group_id).join(Group,
                                                                                                Group.id == group_id).all()
-    print(users)
-    users = [{'id': user[0], 'name': user[1], 'group_name': user[2]}
-             for user in users]
+    users = [{'id': user[0], 'name': user[1], 'group_name': user[2]} for user in users]
     return jsonify({'users': users}), 200
 
 
 @blueprint.route('/api/user/<user_id>', methods=['GET'])
 def user_by_id(user_id):
-    if_user_not_found_by_id(user_id)
+    User.if_user_not_found_by_id(user_id)
     sess = db_session.create_session()
     user = sess.query(User.id, User.login, Group.id, Group.name).filter(User.id == user_id).join(Group,
                                                                                                  Group.id == User.group_id).first()
-    print(user)
     return jsonify({'id': user[0], 'login': user[1], 'group_id': user[2], 'group_name': user[3]}), 200
 
 
 @blueprint.route('/api/user/', methods=['POST'])
 def register_user():
     req = request.get_json(force=True)
-    if_user_already_created(req['login'])
+    User.if_user_already_created(req['login'])
     sess = db_session.create_session()
     user = User(
         login=req['login'],
@@ -89,19 +49,10 @@ def register_user():
 @blueprint.route('/api/user/', methods=['GET'])
 def login_user():
     req = request.get_json(force=True)
-    if_user_not_found_by_name(req['login'])
+    User.if_user_not_found_by_name(req['login'])
     sess = db_session.create_session()
-    user = sess.query(User.id, User.password).filter(
-        User.login == req['login']).all()[0]
-    access_token = create_access_token(identity=user[0])
+    user = sess.query(User.id, User.password).filter(User.login == req['login']).all()[0]
     if check_password(req['password'], user[1]):
-        return jsonify(access_token=access_token), 200
+        return jsonify({'id': user[0]}), 200
     else:
         abort(401, 'Password - login pair is incorrect')
-
-
-@blueprint.route('/api/user/test', methods=['GET'])
-@jwt_required()
-def test():
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
--- data/models/Users.py
+++ data/models/Users.py
@@ -13,3 +13,30 @@ class User(SqlAlchemyBase, SerializerMixin):  # название модели
     login = sqlalchemy.Column(sqlalchemy.Text)
     password = sqlalchemy.Column(sqlalchemy.Text)
     group_id = sqlalchemy.Column(sqlalchemy.Integer, ForeignKey('groups.id'))
+
+    def if_user_not_found_by_id(user_id):
+        sess = db_session.create_session()
+        user = sess.query(User).get(user_id)
+        if not user:
+            abort(404, 'User id not found')
+
+
+    def if_user_not_found_by_name(user_name):
+        sess = db_session.create_session()
+        user = sess.query(User).filter(User.login == user_name).all()
+        if not user:
+            abort(404, 'User name not found')
+
+
+    def if_user_not_found_by_group(user_group_id):
+        sess = db_session.create_session()
+        user = sess.query(User).filter(User.group_id == user_group_id).all()
+        if not user:
+            abort(404, 'User with this group_id name not found')
+
+
+    def if_user_already_created(user_name):
+        sess = db_session.create_session()
+        user = sess.query(User).filter(User.login == user_name).all()
+        if user:
+            abort(403, 'User already created')
