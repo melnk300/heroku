0e65ddf256a042375d43b24459ccd39ec92a36a8
melnk300 <melnk300@gmail.com>
2021-04-26 23:46:36 +0500

    A bit refactor for vk bot

--- api/vk_api.py
+++ api/vk_api.py
@@ -1,9 +1,11 @@
+import random
 from flask import jsonify, abort, request, Blueprint
-import vkapi
+import vk
 from flask_cors import cross_origin
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token, create_refresh_token, \
     set_refresh_cookies, set_access_cookies, unset_jwt_cookies
 from pprint import pprint
+from utils.cfg import CONFIG
 
 from utils.utils import hash_password, check_password
 
@@ -16,4 +18,14 @@ blueprint = Blueprint('vk_api', __name__)
 
 @blueprint.route('/api/vk/callbackreg', methods=['POST'])
 def callback_reg():
-    return 'bcc0e9d7'
+    data = request.data
+    token = CONFIG.VK_TOCKEN
+    if 'type' not in data.keys():
+        return 'not vk'
+    elif data['type'] == 'message_new':
+        session = vk.Session()
+        api = vk.API(session, v='5.110')
+        user_id = data['object']['message']['from_id']
+        api.messages.send(access_token=token, user_id=str(user_id), message='Привет, я новый бот!',
+                          random_id=random.getrandbits(64))
+        return 'ok'
--- utils/cfg.py
+++ utils/cfg.py
@@ -6,6 +6,8 @@ class DevConfig(Config):
     SECRET = ''
     host = '34.66.70.1'
     JWT_SECRET = 'jwtsecretrusmwrkk'
+    VK_SECRET = 'ZVqujr0gK3Vk8MuzjdiUYoEP8BVw5hzzC3HYkBY8'
+    VK_TOCKEN = '2e0bdd87033cc895acaa9d86770a4db77f639492a5f6a493f8f8562892f791feb9a6e951b54908d2d35d1'
     PG = {
         'database': 'diary',
         'db_user': 'postgres',
