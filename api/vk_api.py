616540fc55d288b9dc9e2bdce8e60412d67bcbb9
melnk300 <melnk300@gmail.com>
2021-04-27 23:31:44 +0500

    Added !дз point in api

--- api/vk_api.py
+++ api/vk_api.py
@@ -1,5 +1,6 @@
 import random
 from flask import jsonify, abort, request, Blueprint
+from datetime import datetime as dt
 import vk
 from pprint import pprint
 from utils.cfg import CONFIG
@@ -10,6 +11,7 @@ from utils.utils import hash_password, check_password, send_message
 
 from data import db_session
 from data.models.Users import User
+from data.models.Tasks import Task
 from data.models.Groups import Group
 from data.models.VkUsers import VkUser
 
@@ -27,7 +29,7 @@ def callback_reg():
         text = data['object']['body'].split()
         if text[0] in ["!группа", "!гр"]:
             sess = db_session.create_session()
-            if Group.if_group_already_created('_'.join(text[1::])):
+            if Group.if_group_already_created__vk('_'.join(text[1::])):
                 group_id = sess.query(Group.id).filter(Group.name == '_'.join(text[1::])).first()[0]
                 user_group = VkUser(
                     id=user_id,
@@ -39,6 +41,16 @@ def callback_reg():
             else:
                 send_message(user_id, 'Такой группы не существует')
                 return '404'
+        elif text[0] in ["!дз"]:
+            sess = db_session.create_session()
+            if not text[1]:
+                date = dt.now().strftime('%Y-%m-%d')
+            else:
+                date = text[1]
+            user_group_id = sess.query(VkUser.group_id).get(user_id)
+            tasks = sess.query(Task.subject, Task.author, Task.task).filter(Task.group_id == user_group_id, Task.date_task == date)
+            send_message(user_id, '\n'.join([f'{task[0]} @{task[1]}\n{task[2]}' for task in tasks]))
+            return '200'
     else:
         return 'ok'
 
